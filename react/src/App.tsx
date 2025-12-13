c5d2337f9a7d89da0e6f9fd713a24aa57c0ae781
Windler <Windler00@gmail.com>
2025-12-13 12:40:31 +0500

    added react router

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<div></div>} />
+        <Route path="*" element={<div></div>} />
+      </Routes>
     </>
   )
 }
